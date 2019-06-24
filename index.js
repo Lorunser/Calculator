4569f6848eb6de30e23b98aa97cd712bf264e3fa
Lorunser <ltray99@gmail.com>
2019-06-24 15:10:26 +0100

    Custom operator calculator

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
+function getFloat(prompt){
+    console.log(prompt + " :");
+    var ans = readline.prompt();
+    ans = parseFloat(ans);
+    return ans;
+}
+
+function getString(prompt){
+    console.log(prompt + " :");
+    var ans = readline.prompt();
+    return ans;
+}
+
+function applyOperator(a, b, op){
+    var c = 0;
+
+    switch(op){
+        case "+":
+            c = a + b;
+            break;
+        case "-":
+            c = a - b;
+            break;
+        case "/":
+            c = a / b;
+            break;
+        case "*":
+            c = a * b;
+        default:
+            console.log("Unsupported operator")
+    }
+
+    return c;
+}
+
+
+function main(){
+    while (true){
+        var op = getString("Operator");
+        
+        var a = getFloat("Number A");
+        var b = getFloat("Number B");  
+
+        var c = applyOperator(a,b,op);
+        console.log("A " + op + " B = " + String(c));
+    }
+}
+
+
 const readline = require('readline-sync');
 console.log("Welcome to the calculator!");
+console.log("--------------------------");
+main();
 
-console.log("Number A: ");
-var a = readline.prompt();
-a = parseFloat(a);
 
-console.log("Number B: ");
-var b = readline.prompt();
-b = parseFloat(b);
-
-var c = a * b;
-console.log("A * B = " + String(c));

22f1c76c5fb1782852f406fc5b8a58558fda0682
Lorunser <ltray99@gmail.com>
2019-06-24 14:54:52 +0100

    Complete simple calculator

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const readline = require('readline-sync');
 console.log("Welcome to the calculator!");
+
+console.log("Number A: ");
+var a = readline.prompt();
+a = parseFloat(a);
+
+console.log("Number B: ");
+var b = readline.prompt();
+b = parseFloat(b);
+
+var c = a * b;
+console.log("A * B = " + String(c));
